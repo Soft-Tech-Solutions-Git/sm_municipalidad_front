ef7a934dbc9eaf684c75fd3f0ddf91cbdd8f112f
rufy90 <carranzarufino90@gmail.com>
2024-06-19 14:38:18 -0600

    adding the newsComponent section

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from './slider/slider.component';
 import { StoriesComponent } from './stories/stories.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
-  declarations: [SliderComponent, StoriesComponent],
+  declarations: [SliderComponent, StoriesComponent, NewsComponent],
   imports: [CommonModule],
-  exports: [SliderComponent, StoriesComponent],
+  exports: [SliderComponent, StoriesComponent, NewsComponent],
 })
 export class SharedModule {}
