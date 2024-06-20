1fddcb85072b1136929a00dfdb136b4a0821ada5
rufy90 <carranzarufino90@gmail.com>
2024-06-20 11:12:48 -0600

    adding the navBar and some changes

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SliderComponent } from './slider/slider.component';
 import { StoriesComponent } from './stories/stories.component';
 import { NewsComponent } from './news/news.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [SliderComponent, StoriesComponent, NewsComponent],
+  declarations: [
+    SliderComponent,
+    StoriesComponent,
+    NewsComponent,
+    NavBarComponent,
+  ],
   imports: [CommonModule],
-  exports: [SliderComponent, StoriesComponent, NewsComponent],
+  exports: [SliderComponent, StoriesComponent, NewsComponent, NavBarComponent],
 })
 export class SharedModule {}
