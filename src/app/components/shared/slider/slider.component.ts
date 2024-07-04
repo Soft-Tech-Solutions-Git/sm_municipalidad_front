3fe846cdf8a908d1a23966fe2d4bdb893b4da736
rufy90 <carranzarufino90@gmail.com>
2024-07-03 21:49:16 -0600

    adding the logic component

--- src/app/components/shared/slider/slider.component.ts
+++ src/app/components/shared/slider/slider.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as Glide from '@glidejs/glide'; //importacion normal sugerida da error por ser legacyVersion
+import { AdaptativeService } from 'src/app/services/adaptative.service';
 
 @Component({
   selector: 'app-slider',
@@ -20,16 +21,53 @@ export class SliderComponent implements AfterViewInit {
       // Configura las opciones de Glide aquí
       type: 'carousel',
       perView: 2,
+      startAt: 1,
       focusAt: 'center',
       autoplay: 3000,
-      gap: 5,
+      gap: 250,
       hoverpause: false,
       breakpoints: {
+        1600: {
+          perView: 2,
+          gap: 200,
+        },
         1000: {
           perView: 1,
         },
       },
     });
     this.glide.mount(); // Inicializa Glide
+  }
+
+  public posicion: string;
+
+  constructor(private AdaptativeService: AdaptativeService) {
+    this.posicion = 'center';
+  }
+
+  ngOnInit(): void {
+    if (this.AdaptativeService.sizeDisplay === 'web') {
+      console.log('Desktop');
+
+      // this.glide = new Glide.default('.glide', {
+      //   perView: 2,
+      //   gap: 250,
+      // });
+    }
+    if (this.AdaptativeService.sizeDisplay === 'tablet') {
+      console.log('Tablet');
+
+      // this.glide = new Glide.default('.glide', {
+      //   perView: 2,
+      //   gap: 200,
+      // });
+    }
+    if (this.AdaptativeService.sizeDisplay === 'phone') {
+      console.log('Phone');
+
+      // this.glide = new Glide.default('.glide', {
+      //   perView: 1,
+      // });
+    }
   }
 }
